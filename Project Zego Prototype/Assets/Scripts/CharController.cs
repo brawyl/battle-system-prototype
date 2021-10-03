09b93fe891057f73c8038caf97f70b285c44a160
brawyl <bwylie@ualberta.ca>
2021-10-03 17:51:24 -0600

    Setup UI and gameobjects for battle system

---
+++ Project Zego Prototype/Assets/Scripts/CharController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class CharController : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text charNameText;
+    [SerializeField]
+    private Slider hpSlider;
+    [SerializeField]
+    private GameObject characterObject;
+
+    public string charName;
+    public int charHPMax;
+    public int charHPCurrent;
+    public int charStrength;
+    public int charDefense;
+    public int charSpeed;
+    public bool charAlive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charAlive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        charHPCurrent -= damage;
+        charHPCurrent = charHPCurrent < 0 ? 0 : charHPCurrent;
+        hpSlider.value = charHPCurrent / charHPMax;
+
+        if (charHPCurrent <= 0)
+        {
+            charAlive = false;
+        }
+    }
+
+    void damageTarget(int damage, GameObject target)
+    {
+        target.GetComponent<CharController>().TakeDamage(damage);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # project-zego-prototype
 This repo is for prototypes of the various systems to be used in project zego.
+
+Battle System prototype screenshot:
+![battle system](/screenshot.png)
