f97d98de43982f23746bb68a272dc9449c054bf7
brawyl <bwylie@ualberta.ca>
2023-08-26 19:16:19 -0600

    Removes unused imports in scripts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 using System.Linq;
-using UnityEditor.Experimental.GraphView;
 
 public class GameManager : MonoBehaviour
 {
